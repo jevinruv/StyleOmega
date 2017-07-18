fb378eea9980092307a389026240832f7533985e
Jevin Ruvin <jevinruv@gmail.com>
2017-07-18 21:40:23 +0530

    added home_activity

--- app/src/main/java/com/example/jevin/styleomega/MainActivity.java
+++ app/src/main/java/com/example/jevin/styleomega/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.jevin.styleomega;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
+    }
+
+    public void BtnSignInClicked(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+
     }
 }
