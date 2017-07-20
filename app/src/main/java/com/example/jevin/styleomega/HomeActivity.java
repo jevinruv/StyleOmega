bc387dbf6121f94ddb6c1d81668248aed3e24c27
Jevin Ruvin <jevinruv@gmail.com>
2017-07-20 19:27:44 +0530

    added signup activity and some housekeeping

--- app/src/main/java/com/example/jevin/styleomega/HomeActivity.java
+++ app/src/main/java/com/example/jevin/styleomega/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.jevin.styleomega;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,4 +16,5 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
 }
--- app/src/main/java/com/example/jevin/styleomega/MainActivity.java
+++ app/src/main/java/com/example/jevin/styleomega/MainActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+
+
     }
 
     public void BtnSignInClicked(View view){
         Intent intent = new Intent(this, HomeActivity.class);
+      startActivity(intent);
+
+    }
+
+    public void BtnSignUpClicked(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
 
     }
+
+
+
+
 }
